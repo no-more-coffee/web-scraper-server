292d35f76a628dd51477880545a22dc2405beb88
Vladimir Okonechnikov <vokonechnikov@tetrascience.com>
2023-07-02 11:25:40 +0200

    Add frontend

--- web_scraper_server/main.py
+++ web_scraper_server/main.py
@@ -1,5 +1,8 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+import httpx
 
 from web_scraper_server.storage import crud, models, schemas
 from web_scraper_server.storage.database import engine
@@ -9,8 +12,20 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="web_scraper_server/templates")
 
-@app.get("/", response_model=list[schemas.Apartment])
-def read_items(skip: int = 0, limit: int = 100, db_context: Session = Depends(get_db)):
+
+@app.get("/apartments", response_model=list[schemas.Apartment])
+async def read_items(skip: int = 0, limit: int = 100, db_context: Session = Depends(get_db)):
     with db_context as db:
         return crud.get_items(db, skip=skip, limit=limit)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    url = str(request.url_for("read_items"))
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+
+    items = response.json()
+    return templates.TemplateResponse("index.html", {"request": request, "page": 1, "items": items})
